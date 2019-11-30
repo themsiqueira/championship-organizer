61a75a83c38f1c2cfb8de6f9aa85fe19463eae80
Mateus Siqueira Carneiro <mateussiqueiracarneiro@outlook.com>
2019-11-30 13:03:17 -0300

    Initial Project Config

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    apidoc: {
+      mypp: {
+        src: "app/",
+        dest: "apidoc/",
+        options: {
+          debug: true,
+          includeFilters: [ ".*\\.js$" ],
+          excludeFilters: [ "node_modules/" ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-apidoc');
+};
